78588ef4daeb4d138db5c5c30f2d9391847e6a10
Ant0C <cafeo97@gmail.com>
2023-03-30 16:00:21 +0200

    aggiunto metodo

--- Index.php
+++ Index.php
@@ -10,14 +10,49 @@ class Movie{
         $this->genere = $_genere;
         $this->durata = $_durata;
     }
+
+    public function __toString(){
+        return $this->titolo. " " .$this->genere. " " .$this->durata;
+    }
 }
 
-$Film1 = new Movie("Smetto quando voglio","Comico",110);
+$film1 = new Movie("Smetto quando voglio","Comico",110);
 
-$Film2 = new Movie("Profondo rosso","Horror",130);
+$film2 = new Movie("Profondo rosso","Horror",130);
 
-$Film3 = new Movie("La grande bellezza","Drammatico",170);
+$film3 = new Movie("La grande bellezza","Drammatico",170);
 
-var_dump($Film1,$Film2,$Film3)
+var_dump($film1,$film2,$film3);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+
+<body>
+    <div>
+        <h1>Esercizio OOP</h1>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <?php echo $film1 ?>
+            </li>
+            <li>
+                <?php echo $film2 ?>
+            </li>
+            <li>
+                <?php echo $film3 ?>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
