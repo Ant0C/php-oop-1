57d6cac1608324270a460850b4dee6f80921f8c4
Ant0C <cafeo97@gmail.com>
2023-03-30 14:53:59 +0200

    inserimento del costruttore

--- Index.php
+++ Index.php
@@ -1,26 +1,22 @@
 <?php
 
 class Movie{
+    public $titolo;
+    public $genere;
+    public $durata;
 
+    function __construct($_titolo,$_genere,$_durata){
+        $this->titolo = $_titolo;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+    }
 }
 
-$Film1 = new Movie();
+$Film1 = new Movie("Smetto quando voglio","Comico",110);
 
-    $Film1->titolo = "Smetto quando voglio";
-    $Film1->genere = "Comico";
-    $Film1->durata = 110 ;
+$Film2 = new Movie("Profondo rosso","Horror",130);
 
-$Film2 = new Movie();
-
-    $Film2->titolo = "Profondo rosso";
-    $Film2->genere = "Horror";
-    $Film2->durata = 130 ;
-
-$Film3 = new Movie();
-
-    $Film3->titolo = "La grande bellezza";
-    $Film3->genere = "Drammatico";
-    $Film3->durata = 170 ;
+$Film3 = new Movie("La grande bellezza","Drammatico",170);
 
 var_dump($Film1,$Film2,$Film3)
 
