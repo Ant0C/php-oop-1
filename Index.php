74f76217e1f8fe75a76ef6f903fa5b85d52d43f1
Ant0C <cafeo97@gmail.com>
2023-03-30 14:45:54 +0200

    costruita classe con variabili

---
+++ Index.php
@@ -0,0 +1,27 @@
+<?php
+
+class Movie{
+
+}
+
+$Film1 = new Movie();
+
+    $Film1->titolo = "Smetto quando voglio";
+    $Film1->genere = "Comico";
+    $Film1->durata = 110 ;
+
+$Film2 = new Movie();
+
+    $Film2->titolo = "Profondo rosso";
+    $Film2->genere = "Horror";
+    $Film2->durata = 130 ;
+
+$Film3 = new Movie();
+
+    $Film3->titolo = "La grande bellezza";
+    $Film3->genere = "Drammatico";
+    $Film3->durata = 170 ;
+
+var_dump($Film1,$Film2,$Film3)
+
+?>
