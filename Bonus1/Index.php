0f0d10ff6d18ad9c7f9310525beb35323030a436
Ant0C <cafeo97@gmail.com>
2023-03-30 16:08:30 +0200

    Fix metodo

---
+++ Bonus1/Index.php
@@ -0,0 +1,54 @@
+<?php
+
+class Movie{
+    public $titolo;
+    public $genere;
+    public $durata;
+
+    public function __construct($_titolo,$_genere,$_durata){
+        $this->titolo = $_titolo;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+    }
+}
+
+$film1 = new Movie("Smetto quando voglio","Comico",110);
+
+$film2 = new Movie("Profondo rosso","Horror",130);
+
+$film3 = new Movie("La grande bellezza","Drammatico",170);
+
+var_dump($film1,$film2,$film3);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+
+<body>
+    <div>
+        <h1>Esercizio OOP</h1>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <?php echo $film1 ?>
+            </li>
+            <li>
+                <?php echo $film2 ?>
+            </li>
+            <li>
+                <?php echo $film3 ?>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
